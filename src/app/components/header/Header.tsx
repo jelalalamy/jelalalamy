7a2ae185f769d0e272328e95ee1bd20fa122fcd7
Jeremy La <jeremylai511@gmail.com>
2024-01-26 02:32:15 -0500

    Divide header into components

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,6 +1,16 @@
+import HeaderMain from "./HeaderMain"
+import HeaderNav from "./HeaderNav"
+import HeaderSocials from "./HeaderSocials"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+      <div>
+        <HeaderMain/>
+        <HeaderNav/>
+      </div>
+      <HeaderSocials/>
+    </header>
   )
 }
 
---
+++ src/app/components/header/HeaderMain.tsx
@@ -0,0 +1,13 @@
+const HeaderMain = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+        <a href="/">Jeremy La</a>
+      </h1>
+      <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Software Developer</h2>
+      <p className="mt-4 max-w-xs leading-normal">I build pixel-perfect, engaging, and accessible digital experiences.</p>
+    </div>
+  )
+}
+
+export default HeaderMain
