dfffab1a9bb7630c61b3dab5fe0592c991f95296
Jeremy La <jeremylai511@gmail.com>
2024-01-27 00:08:57 -0500

    Started experience section

--- src/app/components/main/Main.tsx
+++ src/app/components/main/Main.tsx
@@ -1,9 +1,11 @@
 import About from "./About"
+import Experience from "./Experience"
 
 const Main = () => {
   return (
     <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
-      <About />
+      <About/>
+      <Experience/>
     </main>
   )
 }
