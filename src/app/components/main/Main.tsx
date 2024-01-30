f8444df6095e09c2d00a10aa30690066c42e49f0
Jeremy La <jeremylai511@gmail.com>
2024-01-30 00:32:18 -0500

    Started projects section

--- src/app/components/main/Main.tsx
+++ src/app/components/main/Main.tsx
@@ -1,11 +1,13 @@
 import About from "./About"
 import Experience from "./Experience"
+import Projects from "./Projects"
 
 const Main = () => {
   return (
     <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
       <About/>
       <Experience/>
+      <Projects/>
     </main>
   )
 }
