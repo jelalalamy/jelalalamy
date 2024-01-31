ad9695b3362b2acd18ff9b27a7fb08e51b2b1e96
Jeremy La <jeremylai511@gmail.com>
2024-01-31 15:24:39 -0500

    Structure experiences using components

---
+++ src/app/components/main/TechnologyBubble.tsx
@@ -0,0 +1,9 @@
+const TechnologyBubble = ({technology} : {key: string, technology: string}) => {
+  return (
+    <li className="mr-1.5 mt-2">
+      <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">{technology}</div>
+    </li>
+  )
+}
+
+export default TechnologyBubble
