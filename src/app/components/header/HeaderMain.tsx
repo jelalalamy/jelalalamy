4a5a62d76a638af68c7a1ec41117eeaa8b0a1525
Jeremy La <jeremylai511@gmail.com>
2024-02-04 22:18:03 -0500

    Updated header text

--- src/app/components/header/HeaderMain.tsx
+++ src/app/components/header/HeaderMain.tsx
@@ -5,7 +5,7 @@ const HeaderMain = () => {
         <a href="/">Jeremy La</a>
       </h1>
       <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Software Developer</h2>
-      <p className="mt-4 max-w-xs leading-normal">I build pixel-perfect, engaging, and accessible digital experiences.</p>
+      <p className="mt-4 max-w-xs leading-normal">I build pixel-perfect digital experiences, blending functionality and efficiency with user-centric design.</p>
     </div>
   )
 }
