dc3e0b56ff10ae1f8600d225fb1058715bcd4e94
Jeremy La <jeremylai511@gmail.com>
2026-08-17 01:24:34 -0700

    Update About.tsx

--- src/app/components/main/About.tsx
+++ src/app/components/main/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
           .
         </p>
         <p className="mb-4">
-          I&apos;m currently working at Google as a Software Engineer on the Search Maps team. The map that you see on Search? That was us.
+          I&apos;m currently working at Google as a Software Engineer building amazing experiences on Search. The map that you see on Search? That was us.
         </p>
         <p className="mb-4">
           When I&apos;m not working, I&apos;m usually playing badminton, hanging out with friends, watching food videos, or relaxing while listening to some of my&nbsp;
